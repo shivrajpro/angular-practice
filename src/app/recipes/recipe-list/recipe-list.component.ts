724090a47f533038c48094d0cf841af23a4cae5d
shivraj <shivrajhulbatte@gmail.com>
2020-09-01 02:23:39 +0530

    Section 3 : Course Project Basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[]=[
+    new Recipe("Duck", "Fried Duck", "https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg"),
+    new Recipe("Duck", "Fried Duck", "https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg"),
+    new Recipe("Duck", "Fried Duck", "https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg"),
+    new Recipe("Duck", "Fried Duck", "https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_960_720.jpg")
+  ];
 
   constructor() { }
 

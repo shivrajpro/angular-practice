5409b9c2b48974947fd16bf9487d66e41ed3cdba
shivraj <shivrajhulbatte@gmail.com>
2020-09-01 16:19:09 +0530

    @Input decorator usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import {  FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementsComponent } from './server-elements/server-elements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent
+    ServersComponent,
+    CockpitComponent,
+    ServerElementsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server-elements/server-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-elements',
+  templateUrl: './server-elements.component.html',
+  styleUrls: ['./server-elements.component.css']
+})
+export class ServerElementsComponent implements OnInit {
+  @Input('smelement')element: {type:string, name:string,content:string};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

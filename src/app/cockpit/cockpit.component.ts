02d222aa5aaddde44f9f7be4edddc1f16607a863
shivraj <shivrajhulbatte@gmail.com>
2020-09-01 20:16:52 +0530

    video 75: using @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -10,7 +10,9 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  
+  @ViewChild('serverContentInput',{static:true}) serverContent:ElementRef;
 
   constructor() { }
 
@@ -20,14 +22,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput:HTMLInputElement) {
     this.serverCreated.emit({
       serverName:nameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContent.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput:HTMLInputElement) {
     this.bluePrintCreated.emit({
       serverName:nameInput.value,
-      serverContent:this.newServerContent
+      serverContent:this.serverContent.nativeElement.value
     });
   }  
 }

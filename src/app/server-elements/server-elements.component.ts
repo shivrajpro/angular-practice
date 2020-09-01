3a2e5ef90f5c3b2a0868baa7065bf2524b218544
shivraj <shivrajhulbatte@gmail.com>
2020-09-01 20:24:22 +0530

    video 76 : ng-content directive

--- src/app/server-elements/server-elements.component.ts
+++ src/app/server-elements/server-elements.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-server-elements',
   templateUrl: './server-elements.component.html',
-  styleUrls: ['./server-elements.component.css']
-  // encapsulation:ViewEncapsulation.None
+  styleUrls: ['./server-elements.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class ServerElementsComponent implements OnInit {
   @Input('srvElement') element: { type: string, name: string, content: string };

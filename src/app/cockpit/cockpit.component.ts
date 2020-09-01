b8f6ab06b689da8d61402394fe0fd7c7aa88df31
shivraj <shivrajhulbatte@gmail.com>
2020-09-01 20:01:38 +0530

    View Encapsulation and using local references in templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
 
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -17,16 +17,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput:HTMLInputElement) {
     this.serverCreated.emit({
-      serverName:this.newServerName,
+      serverName:nameInput.value,
       serverContent:this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput:HTMLInputElement) {
     this.bluePrintCreated.emit({
-      serverName:this.newServerName,
+      serverName:nameInput.value,
       serverContent:this.newServerContent
     });
   }  

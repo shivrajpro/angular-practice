fc9098b088e48aa78f7609f13e187fa88f1bc953
shivraj <shivrajhulbatte@gmail.com>
2020-09-01 19:17:37 +0530

    @Output decorator and custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server',name:'TestServer',content:'Just a test'}];
+
+  onServedAdded(serverData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(serverData:{serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }    
 }

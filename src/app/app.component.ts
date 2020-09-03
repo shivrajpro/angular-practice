d379439d3c00ae35eab60e46cd30d6a8be1cf73d
shivraj <shivrajhulbatte@gmail.com>
2020-09-03 20:57:10 +0530

    perform cleanup to start Section 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  serverElements = [{type:'server',name:'TestServer',content:'Just a test'}];
-
-  onServedAdded(serverData:{serverName:string, serverContent:string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onBluePrintAdded(serverData:{serverName:string, serverContent:string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-  
-  onChangeFirst(){
-    this.serverElements[0].name="Changed!";
-  }
-
-  onDeleteFirst(){
-    this.serverElements.splice(0,1);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 import {  FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementsComponent } from './server-elements/server-elements.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServersComponent,
-    CockpitComponent,
-    ServerElementsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

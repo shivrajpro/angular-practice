34c766016db12304e9d2af1cda3f33f768b9d2c4
shivraj <shivrajhulbatte@gmail.com>
2020-09-05 01:08:46 +0530

    add ingredients to Recipe

--- src/app/recipes/services/recipe.service.ts
+++ src/app/recipes/services/recipe.service.ts
@@ -1,14 +1,41 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from "../../recipes/recipe.model";
+import { Ingredient } from 'src/app/shared/Ingredient.model';
 
 export class RecipeService{
 
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes : Recipe[]=[
-        new Recipe("Chapati", "Fried Chapati", "https://cdn.pixabay.com/photo/2014/10/15/05/05/chapati-489254_960_720.jpg"),
-        new Recipe("Paneer", "Palak Paneer", "https://media.istockphoto.com/photos/palak-paneer-at-grey-concrete-background-picture-id1166167732"),
-        new Recipe("Pizza", "Margherita", "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg")
+        new Recipe(
+            "Chapati",
+            "Fried Chapati",
+            "https://cdn.pixabay.com/photo/2014/10/15/05/05/chapati-489254_960_720.jpg",
+            [
+              new Ingredient("Wheat",2),
+              new Ingredient("Oil",1)
+            ]
+          ),
+
+        new Recipe(
+            "Paneer",
+            "Palak Paneer",
+            "https://media.istockphoto.com/photos/palak-paneer-at-grey-concrete-background-picture-id1166167732",
+            [
+              new Ingredient("Milk",2),
+              new Ingredient("Palak",5)
+            ]
+          ),
+
+        new Recipe(
+            "Pizza",
+            "Margherita",
+            "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg",
+            [
+              new Ingredient("Maida",4),
+              new Ingredient("Tomato",1)
+            ]
+          )
       ];
     
     getRecipes(){

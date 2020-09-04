bf4d17efa1c8b9362aa0c86eb22946aa00890d09
shivraj <shivrajhulbatte@gmail.com>
2020-09-04 22:53:49 +0530

    Section 9 : DONE!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { AccountsService } from "./services/accounts.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-  // providers: [AccountsService]
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
 export class AppComponent implements OnInit{
   accounts: {name:string, status: string}[] = [];
@@ -16,4 +16,13 @@ export class AppComponent implements OnInit{
     this.accounts =  this.accountsService.accounts;
   }
 
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accountsService.addAccount(newAccount.name, newAccount.status);
+    // this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accountsService.updateStatus(updateInfo.id, updateInfo.newStatus);
+    // this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
 }

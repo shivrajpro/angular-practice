50ed0218393162f962e465dd471879769220f1ff
shivraj <shivrajhulbatte@gmail.com>
2020-09-05 12:40:40 +0530

    getting ready for Section 11 : Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountsService } from "./services/accounts.service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [AccountsService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  accounts: {name:string, status: string}[] = [];
-
-  constructor(private accountsService: AccountsService){}
-
-  ngOnInit(){
-    this.accounts =  this.accountsService.accounts;
-  }
-
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accountsService.addAccount(newAccount.name, newAccount.status);
-    // this.accounts.push(newAccount);
-  }
-
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accountsService.updateStatus(updateInfo.id, updateInfo.newStatus);
-    // this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
+export class AppComponent {
 }
--- src/app/services/logging.service.ts
+++
@@ -1,5 +0,0 @@
-export class LoggingService{
-    logStatusToConsole(status: string){
-        console.log('A server status changed, new status: ' + status);
-    }
-}

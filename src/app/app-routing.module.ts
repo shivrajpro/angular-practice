68b7f89c04429ab343bf04643f0b34b06308eae0
shivraj <shivrajhulbatte@gmail.com>
2020-09-06 12:13:17 +0530

    Outsourcing or Separating AppRoutingModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+    {
+      path: '',
+      component: HomeComponent
+    },
+    {
+      path: 'servers',
+      component: ServersComponent,
+      children: [
+        {
+          path: ':id',
+          component: ServerComponent
+        },
+        {
+          path: ':id/edit',
+          component: EditServerComponent
+        },
+      ]
+    },
+    {
+      path: 'users',
+      component: UsersComponent,
+      children: [
+        {
+          path: ':id/:name',
+          component: UserComponent
+        }
+      ]
+    },
+    {
+      path: 'not-found',
+      component: PageNotFoundComponent
+    },
+    //must be at end
+    {
+      path: '**',
+      redirectTo: 'not-found'
+    }
+  ]
+  
+@NgModule({
+    imports:[
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes } from "@angular/router";
-import { RouterModule } from "@angular/router";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,48 +11,9 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'servers',
-    component: ServersComponent,
-    children: [
-      {
-        path: ':id',
-        component: ServerComponent
-      },
-      {
-        path: ':id/edit',
-        component: EditServerComponent
-      },
-    ]
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      {
-        path: ':id/:name',
-        component: UserComponent
-      }
-    ]
-  },
-  {
-    path: 'not-found',
-    component: PageNotFoundComponent
-  },
-  //must be at end
-  {
-    path: '**',
-    redirectTo: 'not-found'
-  }
-]
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
@@ -68,7 +29,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]

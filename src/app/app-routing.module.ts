e97c956b7e9b14f8caf64001ea0a80e3ad151b94
shivraj <shivrajhulbatte@gmail.com>
2020-09-07 12:55:53 +0530

    Section 13: Understanding Observables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,74 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserComponent } from './user/user.component';
 
-import { AuthGuard } from "./auth-guard.service";
-import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { ServerResolver } from './servers/server/server-resolver.service';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'servers',
-    canActivateChild: [AuthGuard],
-    //   canActivate: [AuthGuard],
-
-    component: ServersComponent,
-    children: [
-      {
-        path: ':id',
-        component: ServerComponent,
-        resolve: { myServer: ServerResolver }
-      },
-      {
-        path: ':id/edit',
-        component: EditServerComponent,
-        canDeactivate: [CanDeactivateGuard]
-      },
-    ]
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      {
-        path: ':id/:name',
-        component: UserComponent
-      }
-    ]
-  },
-  {
-    path: 'not-found',
-    component: ErrorPageComponent,
-    data: { message: 'ERROR 404 : Page NOT Found' }
-  },
-  //must be at end
-  {
-    path: '**',
-    redirectTo: 'not-found'
-  }
-]
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, {useHash: true})
+    RouterModule.forRoot(routes)
   ],
   exports: [
     RouterModule
   ]
 })
-
 export class AppRoutingModule {
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +7,9 @@ import { AuthService } from '../services/auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  onLoadServer(id: number) {
-    this.router.navigate(
-      ['/servers', id, 'edit'],
-      {
-        queryParams: { allowEdit: 1 },
-        fragment: 'loading'
-      }
-    );
-  }
-
-  onLogin(){
-    this.authService.login();
-  }
-
-  onLogout(){
-    this.authService.logout();
-  }
 }

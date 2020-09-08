1ada9101cc0cbc41ba1022cf379cad0b2a857d54
shivraj <shivrajhulbatte@gmail.com>
2020-09-09 00:30:25 +0530

    creating template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,63 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserService } from "./user/user.service";
-import { Subscription } from 'rxjs';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
-  isActivated: boolean = false;
+export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultQue = "teacher"; //one way binded
+  answer = '';
+  genders = ['Male', "Female"];
 
-  constructor(private userService: UserService) {}
-  
-  ngOnInit() {
-    this.userService.activatedEmitter.subscribe(
-      (didActivate)=>{
-        this.isActivated = didActivate;
-      });
-    }
-    
-    ngOnDestroy(): void {
-      this.userService.activatedEmitter.unsubscribe();
-    }
+  submitted:boolean = false;
+
+  user = {
+    username:'',
+    email:'',
+    secretQuestion:'',
+    questionAnswer:'',
+    gender:''
+  };
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+/* 
+    this.signUpForm.form.setValue({
+      userData:{
+        username:suggestedName,
+        email:''
+      },
+      secret:'teacher',
+      questionAns:'',
+      gender:'Male'
+    });
+
+ */    
+    this.signUpForm.form.patchValue({
+      userData:{
+        username:suggestedName
+      }
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit(){
+    this.submitted = true;
+    console.log(this.signUpForm.form.value);
+
+    let signUpFormValues = this.signUpForm.form.value;
+    this.user.username = signUpFormValues.userData.username,
+    this.user.email = signUpFormValues.userData.email;
+    this.user.secretQuestion = signUpFormValues.secret;
+    this.user.questionAnswer = signUpFormValues.questionAns;
+    this.user.gender = signUpFormValues.gender;
+
+    this.signUpForm.form.reset(); //can pass the object as well
+  }
 }

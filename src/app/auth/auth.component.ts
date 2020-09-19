9f2e42313eb10f35109eef55b7a53543f22232a1
shivraj <shivrajhulbatte@gmail.com>
2020-09-19 14:23:28 +0530

    implementing login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,49 +1,53 @@
 import { Component } from "@angular/core";
 import { NgForm } from '@angular/forms';
-import { AuthService } from "./auth.service";
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
-    templateUrl:'./auth.component.html',
-    styles:[`
+    templateUrl: './auth.component.html',
+    styles: [`
         input.ng-invalid.ng-touched{
             border : 1px solid red;
         }
     `]
 })
-export class AuthComponent{
-    inLoginMode:boolean = false;
+export class AuthComponent {
+    inLoginMode: boolean = false;
     isLoading: boolean = false;
     error = null;
 
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService) { }
 
-    onSwitchMode(){
+    onSwitchMode() {
         this.inLoginMode = !this.inLoginMode;
     }
 
-    onSubmit(authForm: NgForm){
-        console.log(authForm);
+    onSubmit(authForm: NgForm) {
+        // console.log(authForm);
         this.isLoading = true;
-        if(!authForm.valid)
-        return;
-        if(this.inLoginMode){
-            
-        }else{
-            const email = authForm.value.email;
-            const password =  authForm.value.password;
-            
-            this.authService.signup(email, password)
-            .subscribe(responseData =>{
-                    this.isLoading = false;
-                    console.log(responseData);
-                },errorMsg=>{
-                    this.isLoading = false;
-                    this.error = errorMsg;
-                    console.log(errorMsg);
-                });
 
+        const email = authForm.value.email;
+        const password = authForm.value.password;
+
+        let authObs: Observable<AuthResponseData>;
+
+        if (!authForm.valid)
+            return;
+        if (this.inLoginMode) {
+            authObs = this.authService.login(email, password);
+        } else {
+            authObs = this.authService.signup(email, password)
             authForm.reset();
         }
+
+        authObs.subscribe(responseData => {
+            this.isLoading = false;
+            console.log(responseData);
+        }, errorMsg => {
+            this.isLoading = false;
+            this.error = errorMsg;
+            console.log(errorMsg);
+        });
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,40 +1,67 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;        //	A Firebase Auth ID token for the newly created user.
     email: string;          //  The email for the newly created user.
     refreshToken: string;   //	A Firebase Auth refresh token for the newly created user.
     expiresIn: string;      //	The number of seconds in which the ID token expires.
-    localId: string         //  The uid of the newly created use
+    localId: string;        //  The uid of the newly created use
+    registered?: boolean;   //  Whether the email is for an existing account.
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    webApiKey = "AIzaSyADY1P8OfM_kDeXGmheKpFLuHaoMN9h7rM";
+
+    signUpUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+
+    loginUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
 
     constructor(private http: HttpClient) { }
 
-    signup(email: string, passowrd: string) {
-        return this.http.post<AuthResponseData>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyADY1P8OfM_kDeXGmheKpFLuHaoMN9h7rM",
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>(this.signUpUrl+this.webApiKey,
             {
                 email: email,
-                password: passowrd,
+                password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = "An Unknown Error Occurred!";
+        ).pipe(catchError(this.handleError));
+    }
 
-            if (!errorRes.error || !errorRes.error.error)
-                return throwError(errorMessage);
 
-            switch (errorRes.error.error.message) {
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'This email id already exists!'
+    login(email: string, password: string){
+        return this.http.post<AuthResponseData>(this.loginUrl+this.webApiKey,
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
             }
+        ).pipe(catchError(this.handleError));        
+    }
+    
+    private handleError(errorRes: HttpErrorResponse){
+        console.log("Error", errorRes);
 
+        let errorMessage = "An Unknown Error Occurred!";
+    
+        if (!errorRes.error || !errorRes.error.error)
             return throwError(errorMessage);
-        }));
+    
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email Id already exists!';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'Incorrect Password! Try Again';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'Email Id not registered. Please try to sign up!';
+        }
+    
+        return throwError(errorMessage);
     }
 }

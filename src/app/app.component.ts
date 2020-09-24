5801d239e1156292509e6bb25c1e9a090e4b62c6
shivraj <shivrajhulbatte@gmail.com>
2020-09-25 00:25:24 +0530

    animate addition and deletion of item in list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,43 +4,65 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations:[
-    trigger('divState',[
+  animations: [
+    trigger('divState', [
       state('normal', style({
-        'background-color':'red',
-        transform:'translateX(0)'
+        'background-color': 'red',
+        transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        backgroundColor:'blue',
-        transform:'translateX(100px)'
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
       })),
       transition('normal <=> highlighted', animate(300)),
       // transition('normal => highlighted', animate(300)),
       // transition('highlighted => normal', animate(800))
     ]),
-    trigger('wildState',[
+
+    trigger('wildState', [
       state('normal', style({
-        'background-color':'red',
-        transform:'translateX(0) scale(1)'
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
       })),
       state('highlighted', style({
-        backgroundColor:'blue',
-        transform:'translateX(100px) scale(1)'
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
       })),
       state('shrunken', style({
-        backgroundColor:'green',
-        transform:'translateX(0) scale(0.5)'
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
       })),
       transition('normal <=> highlighted', animate(300)),
       transition('shrunken <=> *', [
         style({
-          'background-color':'orange'
+          'background-color': 'orange'
         }),
         animate(1000, style({
-          borderRadius:'50px'
+          borderRadius: '50px'
         })),
         animate(500)
       ])// *  is wildcard to state shrunken to any
+    ]),
+
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      //void means no state
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(400)]
+      ),
+      transition('* => void', [
+        animate(400),style({
+          transform:'translate(100px)',
+          opacity:0
+        })
+      ])
     ])
   ]
 })
@@ -50,15 +72,20 @@ export class AppComponent {
 
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAnimate() {
-      this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
-      this.wild == 'normal' ? this.wild = 'highlighted' : this.wild = 'normal';
-    } 
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wild == 'normal' ? this.wild = 'highlighted' : this.wild = 'normal';
+  }
 
-    onShrink() {
-      this.wild = 'shrunken';// ? this.wild = 'normal': this.wild = 'shrunken';
-    }
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onShrink() {
+    this.wild = 'shrunken';// ? this.wild = 'normal': this.wild = 'shrunken';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }

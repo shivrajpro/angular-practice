862110b61eed37ee893d9703128139a19cb005ff
shivraj <shivrajhulbatte@gmail.com>
2020-09-26 13:01:40 +0530

    setup for Section 27: adding offline capabilities with service workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,21 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Post } from "./post.model";
-import { PostsService } from "./posts.service";
-import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [PostsService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  @ViewChild('postForm') postForm: NgForm;
+export class AppComponent implements OnInit {
+  posts: Post[] = [];
 
-  error = null;
-  loadedPosts: Post[] = [];
-  isFetching: boolean = false;
-  errorSubscription: Subscription;
-
-  constructor(private postsService: PostsService) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getPosts();
-    this.errorSubscription = this.postsService.error.subscribe(error=>{
-      this.error = error;
-    });
-  }
-  
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    this.postsService.createAndStore(postData.title, postData.content);
-    this.postForm.reset();
-  }
-  
-  onFetchPosts() {
-    this.getPosts();
-  }
-
-  onClearPosts() {
-    this.postsService.deleteAllPosts().subscribe(()=>{
-      this.loadedPosts= [];
-      
-    });
-  }
-
-  getPosts(){
-    this.isFetching = true;
-    this.postsService.fetchPosts().subscribe((posts)=>{
-      this.loadedPosts = posts;
-      this.isFetching = false;
-    },(errorResponse)=>{
-      this.isFetching = false;
-      this.error = errorResponse.error.error;
-    });
-  }
-
-  onHandleError(){
-    this.error = null;
-  }
-
-  ngOnDestroy(){
-    this.postsService.error.unsubscribe();
+    this.http
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
   }
 }
--- src/app/post.model.ts
+++ src/app/post.model.ts
@@ -1,5 +1,6 @@
 export interface Post {
-    id?: string,
-    title: string,
-    content: string
+  title: string;
+  body: string;
+  id: number;
+  userId: number;
 }

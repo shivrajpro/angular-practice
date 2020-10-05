713cda5b86ad3931a8fa7dc323dc04d644e439d0
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2020-10-06 01:04:52 +0530

    video 425: tesing dependencies

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -1,10 +1,4 @@
 /* tslint:disable:no-unused-variable */
 
 import { ReversePipe } from "./reverse.pipe";
-describe('Pipe: ReversePipe', () => {
-  it('should reverse the inputs', () => {
-    let reversePipe = new ReversePipe();
-    expect(reversePipe.transform('hello')).toEqual('olleh');
-  });
 
-});

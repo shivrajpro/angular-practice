c625c73d9f7c4359eb07cfb2e7c7a51c247b55cb
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2020-10-06 00:03:01 +0530

    getting started for Section 28: Unit Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { Post } from './post.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  posts: Post[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.http
-      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
-  }
+export class AppComponent {
+  title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

c5ccb76c94971e2248922fc816f31786a82cb9ce
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2020-10-10 01:03:58 +0530

    done with section 28: unit testing

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -2,3 +2,10 @@
 
 import { ReversePipe } from "./reverse.pipe";
 
+describe('',()=>{
+
+    it('should reverse the characters in string',()=>{
+        let reversePipe = new ReversePipe();
+        expect(reversePipe.transform('shivraj')).toEqual('jarvihs');
+    })
+});
--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -5,21 +5,21 @@ import { UserComponent } from './user.component';
 import { UserService } from "./user.service";
 import { DataService } from "../shared/data.service";
 
-describe('Component: User',()=>{
-    beforeEach(()=>{
+describe('Component: User', () => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [UserComponent]
         });
     })
 
-    it('should create the app',()=>{
+    it('should create the app', () => {
         let fixture = TestBed.createComponent(UserComponent);
         let app = fixture.debugElement.componentInstance;
 
         expect(app).toBeTruthy();
     })
 
-    it('should use the username from service',()=>{
+    it('should use the username from service', () => {
         let fixture = TestBed.createComponent(UserComponent);
         let app = fixture.debugElement.componentInstance;
         let userService = fixture.debugElement.injector.get(UserService);
@@ -27,7 +27,7 @@ describe('Component: User',()=>{
         expect(userService.user.name).toEqual(app.user.name);
     })
 
-    it('should display the username if user is logged in', ()=>{
+    it('should display the username if user is logged in', () => {
         let fixture = TestBed.createComponent(UserComponent);
         let app = fixture.debugElement.componentInstance;
         app.isLoggedIn = true;
@@ -36,11 +36,47 @@ describe('Component: User',()=>{
         expect(compiled.querySelector('p').textContent).toContain(app.user.name);
     })
 
-    it(`should not display the username if user is not logged in`, ()=>{
+    it(`should not display the username if user is not logged in`, () => {
         let fixture = TestBed.createComponent(UserComponent);
         let app = fixture.debugElement.componentInstance;
         fixture.detectChanges();
         let compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
     })
+
+    it('should not return data if not called asynchronously', () => {
+        let fixture = TestBed.createComponent(UserComponent);
+        let app = fixture.debugElement.componentInstance;
+        let dataService = fixture.debugElement.injector.get(DataService);
+        let spy = spyOn(dataService, 'getDetails').and.returnValue(Promise.resolve('Data'));
+        fixture.detectChanges();
+        expect(app.data).toBe(undefined);
+    })
+
+    it('should return data if called asynchronously', async(() => {
+        let fixture = TestBed.createComponent(UserComponent);
+        let app = fixture.debugElement.componentInstance;
+        let dataService = fixture.debugElement.injector.get(DataService);
+        let spy = spyOn(dataService, 'getDetails').and.returnValue(Promise.resolve('Data'));
+
+        fixture.detectChanges();
+
+        expect(app.data).toBe(undefined);
+        fixture.whenStable().then(() => {
+            expect(app.data).toBe('Data')
+            }
+        );
+    }))
+
+    it('should return data if called asynchronously', fakeAsync(() => {
+        let fixture = TestBed.createComponent(UserComponent);
+        let app = fixture.debugElement.componentInstance;
+        let dataService = fixture.debugElement.injector.get(DataService);
+        let spy = spyOn(dataService, 'getDetails').and.returnValue(Promise.resolve('Data'));
+
+        fixture.detectChanges();
+
+        tick();
+        expect(app.data).toBe('Data');
+    }))
 });

0dd6b5466c16b9062157a58fe113bb0799e9e75e
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-05-09 15:46:15 +0530

    using enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+enum Directions {
+  Up = "UP",
+  Down = "DOWN",
+  Left = "LEFT",
+  Right = "RIGHT"
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    console.log('>> Directions.Up', Directions.Up);
+  }
+
 }

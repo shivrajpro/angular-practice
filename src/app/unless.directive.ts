48535f4f14b056d80a4db054ed6f2144c58f9b21
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-05-09 20:26:52 +0530

    directives

---
+++ src/app/unless.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  // for structural directives it is important that the method name and selector names are same
+  // here appUnless
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}

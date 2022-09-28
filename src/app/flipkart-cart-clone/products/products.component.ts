3c12597c5404d56eaccf4557ce002add7683b596
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2022-09-28 15:58:56 +0530

    complete add_to_cart

--- src/app/flipkart-cart-clone/products/products.component.ts
+++ src/app/flipkart-cart-clone/products/products.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from "../../services/api.service";
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  productList:any;
+  constructor(private api:ApiService) { }
 
   ngOnInit(): void {
+    this.api.getProducts().subscribe(res=>this.productList=res);
   }
 
 }

80d8494f5cbf31aa7d78880b667b6092e4110fdf
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2022-09-29 15:09:41 +0530

    design cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './flipkart-cart-clone/products/products.component';
+import { CartComponent } from './flipkart-cart-clone/cart/cart.component';
 import { QuestionComponent } from './quiz-project/question/question.component';
 import { WelcomeComponent } from './quiz-project/welcome/welcome.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
@@ -14,11 +16,11 @@ const routes: Routes = [
     path:'reactive',
     component:ReactiveFormsComponent
   },
-  // {
-  //   path:'',
-  //   redirectTo:'welcome',
-  //   pathMatch:'full'
-  // },
+  {
+    path:'',
+    redirectTo:'products',
+    pathMatch:'full'
+  },
   // {
   //   path:'welcome',
   //   component:WelcomeComponent
@@ -27,6 +29,14 @@ const routes: Routes = [
   //   path:'question',
   //   component:QuestionComponent
   // }
+  {
+    path:'products',
+    component:ProductsComponent
+  },
+  {
+    path:'cart',
+    component:CartComponent
+  }
 ];
 
 @NgModule({
